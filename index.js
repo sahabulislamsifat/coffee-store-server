23a93297d67af690e894efc638ddfb7583215021
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-11-30 16:35:41 +0600

    user and password added uri

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 7000;
 
-// Name: coffeeStore
-// Password: 0x1os6fwr8rprj8t
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vu9lo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// const uri =
-//   "mongodb+srv://<db_username>:<db_password>@cluster0.vu9lo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri =
-  "mongodb+srv://coffeeStore:0x1os6fwr8rprj8t@cluster0.vu9lo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
